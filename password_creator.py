cdba47971081a98f7cfe93258ff4d65a9403a1a7
Pepega233 <mokrosfilip@gmail.com>
2026-07-24 12:38:18 +0200

    Cela aplikacia

---
+++ password_creator.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+
+
+
+window = Tk()
+window.title("Password Creator")
+logo = tk.PhotoImage(file="logo.png")
+window.iconphoto(False,logo)
+window.config(bg="white")
+
+
+# Custum Tkinter aby to aj pekne vyzeralo #
+# Zadanie dlzky hesla typ = slider#
+# Vyber co by malo heslo obsahovat#
+# Logika na generovanie hesla#
+# Musi mi to zobrat ake dlhe je to heslo#
+# Co som povyberal z tych checkbuttonov#
+# Stlacim tlacidlo a zacne to generovat heslo#
+# Potom sa heslo zobrazi na screene#
+# Objavia sa 2 tlacidka na save a copy#
+# Tlacidko na generovanie hesla
+# Tlacidko na COPY#
+# Tlacidko na SAVE#
+
+window.mainloop()
+
